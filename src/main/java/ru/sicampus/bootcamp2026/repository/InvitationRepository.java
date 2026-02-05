74a0e688d7217abc0ef93c94e08b0dca5ce28b66
Mekito4u <maks_epifanov_2017@mail.ru>
2026-02-05 13:21:30 +0300

    feat: add Spring Data JPA repositories

--- src/main/java/ru/sicampus/bootcamp2026/repository/InvitationRepository.java
+++ src/main/java/ru/sicampus/bootcamp2026/repository/InvitationRepository.java
@@ -2,9 +2,9 @@ package ru.sicampus.bootcamp2026.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.sicampus.bootcamp2026.entity.User;
+import ru.sicampus.bootcamp2026.entity.Invitation;
+
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
 }
--- src/main/java/ru/sicampus/bootcamp2026/repository/MeetingRepository.java
+++ src/main/java/ru/sicampus/bootcamp2026/repository/MeetingRepository.java
@@ -2,9 +2,9 @@ package ru.sicampus.bootcamp2026.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.sicampus.bootcamp2026.entity.User;
+import ru.sicampus.bootcamp2026.entity.Meeting;
+
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 }
