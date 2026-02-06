c4da8c808f993067264063c51de7e1b4e43ce65a
Mekito4u <maks_epifanov_2017@mail.ru>
2026-02-06 14:58:09 +0300

    feat: add time validation for meetings

--- src/main/java/ru/sicampus/bootcamp2026/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/sicampus/bootcamp2026/exception/handler/GlobalExceptionHandler.java
@@ -4,8 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.sicampus.bootcamp2026.exception.DepartmentNotFoundException;
-import ru.sicampus.bootcamp2026.exception.UserNotFoundException;
+import ru.sicampus.bootcamp2026.exception.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +16,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvitationNotFoundException.class)
+    public ResponseEntity<String> handleInvitationNotFoundException(InvitationNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MeetingNotFoundException.class)
+    public ResponseEntity<String> handleMeetingNotFoundException(MeetingNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidMeetingTimeException.class)
+    public ResponseEntity<String> handleInvalidMeetingTimeException(InvalidMeetingTimeException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/ru/sicampus/bootcamp2026/service/impl/MeetingServiceImpl.java
@@ -0,0 +1,77 @@
+package ru.sicampus.bootcamp2026.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.sicampus.bootcamp2026.dto.CreateMeetingRequestDTO;
+import ru.sicampus.bootcamp2026.dto.MeetingDTO;
+import ru.sicampus.bootcamp2026.entity.Meeting;
+import ru.sicampus.bootcamp2026.entity.User;
+import ru.sicampus.bootcamp2026.exception.MeetingNotFoundException;
+import ru.sicampus.bootcamp2026.exception.UserNotFoundException;
+import ru.sicampus.bootcamp2026.repository.MeetingRepository;
+import ru.sicampus.bootcamp2026.repository.UserRepository;
+import ru.sicampus.bootcamp2026.service.MeetingService;
+import ru.sicampus.bootcamp2026.util.MeetingMapper;
+import ru.sicampus.bootcamp2026.util.TimeValidator;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MeetingServiceImpl implements MeetingService {
+
+    private final MeetingRepository meetingRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public MeetingDTO createMeeting(CreateMeetingRequestDTO request) {
+        TimeValidator.validateMeetingTime(request.getStartTime(), request.getEndTime());
+
+        User organizer = userRepository
+                .findById(request.getOrganizerId())
+                .orElseThrow(UserNotFoundException::new);
+
+        Meeting meeting = new Meeting();
+        meeting.setTitle(request.getTitle());
+        meeting.setDescription(request.getDescription());
+        meeting.setOrganizerId(organizer);
+        meeting.setStartTime(request.getStartTime());
+        meeting.setEndTime(request.getEndTime());
+        meeting.setCreatedAt(LocalDateTime.now());
+
+        Meeting savedMeeting = meetingRepository.save(meeting);
+
+        return MeetingMapper.convertToDto(savedMeeting);
+    }
+
+    @Override
+    public MeetingDTO getMeeting(Long id) {
+        Meeting meeting = meetingRepository
+                .findById(id)
+                .orElseThrow(MeetingNotFoundException::new);
+        return MeetingMapper.convertToDto(meeting);
+    }
+
+    @Override
+    public void deleteMeeting(Long id) {
+        if (!meetingRepository.existsById(id)) {
+            throw new MeetingNotFoundException();
+        }
+        meetingRepository.deleteById(id);
+    }
+
+    @Override
+    public List<MeetingDTO> getUserMeetings(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new RuntimeException("User not found");
+        }
+
+        List<Meeting> meetings = meetingRepository.findByOrganizerId(userId);
+
+        return meetings.stream()
+                .map(MeetingMapper::convertToDto)
+                .collect(Collectors.toList());
+    }
+}
